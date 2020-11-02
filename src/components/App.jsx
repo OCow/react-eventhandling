396d193a5f7d4b4c53e9a3f7ca470cf5771c9784
oliver.kuhn2@gmail.com <oliver.kuhn2@gmail.com>
2020-11-02 20:28:36 +0100

    render button color depending on mouse hover

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [heading, setHeading] = useState("Hello, there!");
+  const [btnColor, setBtnColor] = useState("white");
+
+  function handleClick() {
+    console.log("Button clicked");
+    setHeading("Submitted");
+  }
+
+  function handleMouseOver() {
+    console.log("Mouse over button, mouse over button!!!");
+    setBtnColor("black");
+  }
+
+  function handleMouseLeave() {
+    console.log("The mouse has gone, the mouse has gone!!!");
+    setBtnColor("white");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{heading}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: btnColor }}
+        onMouseLeave={handleMouseLeave}
+        onMouseOver={handleMouseOver}
+        onClick={handleClick}
+      >
+        Submit
+      </button>
     </div>
   );
 }
